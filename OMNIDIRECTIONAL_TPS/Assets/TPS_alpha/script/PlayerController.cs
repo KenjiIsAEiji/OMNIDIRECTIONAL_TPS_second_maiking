cb0cb443863375ea5ac02bbe81d9ba023ecea09c
eiji_miyazawa <b1800722@planet.kanazawa-it.ac.jp>
2019-06-17 23:46:35 +0900

    Unityプロジェクトをマスターに

---
+++ OMNIDIRECTIONAL_TPS/Assets/TPS_alpha/script/PlayerController.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    CharacterController characterController;
+
+    public float MoveSpeed = 10.0f;
+    public float Gravity = 9.8f;
+
+    Vector3 MoveVector = Vector3.zero;
+
+    public Transform CamTransform;
+
+    public Animator animator;
+
+    public Transform bodyTransform;
+    public float turnSpeed;
+
+    public float escapeSpeed = 10;
+    public float jumpSpeed = 10;
+    public bool rolling;
+
+    // Use this for initialization
+    void Start()
+    {
+        characterController = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float Move_x = Input.GetAxis("Horizontal");
+        float Move_z = Input.GetAxis("Vertical");
+
+        if (Move_x != 0 || Move_z != 0)
+        {
+            if (!rolling)
+            {
+                bodyTransform.localRotation = Quaternion.Slerp(
+                    bodyTransform.localRotation,
+                    Quaternion.LookRotation(new Vector3(Move_x, 0, Move_z)),
+                    turnSpeed * Time.deltaTime
+                );
+            }
+
+            float turnAngle = Mathf.Lerp(transform.eulerAngles.y,
+                    CamTransform.eulerAngles.y,
+                    turnSpeed * Time.deltaTime
+                );
+
+            transform.eulerAngles = new Vector3(0, turnAngle, 0);
+
+        }
+
+        if (characterController.isGrounded)
+        {
+            if (rolling)
+            {
+                MoveVector = transform.InverseTransformDirection(bodyTransform.forward) * escapeSpeed;
+            }
+            else
+            {
+                MoveVector = new Vector3(Move_x, 0, Move_z) * MoveSpeed;
+            }
+
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                MoveVector.y = jumpSpeed;
+            }
+        }
+        else
+        {
+            //animator.SetFloat("folling",);
+        }
+        animator.SetBool("IsGround", characterController.isGrounded);
+
+        MoveVector.y -= Gravity * Time.deltaTime;
+
+        animator.SetFloat("speed", new Vector2(Move_x,Move_z).magnitude);
+
+        characterController.Move(transform.TransformDirection(MoveVector * Time.deltaTime));
+    }
+}
